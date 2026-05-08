881d4a9ef54c2a762dc5ff20f6311467011e22fd
gabriel-mkt <marketing01@schrader.com.br>
2026-05-08 15:05:48 -0300

    incluido-exercicios-aula-04

---
+++ Aula_04_Exercicio_01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Analisar Idade
+
+Console.Write("Informe sua Idade: ");
+
+int idadeUsuario = int.Parse(Console.ReadLine());
+
+if (idadeUsuario <= 0)
+{
+    Console.WriteLine("Idade incorreta!");
+}
+else if (idadeUsuario < 18)
+{
+    Console.WriteLine("Menor de Idade");
+}
+else if (idadeUsuario >= 65)
+{
+    Console.WriteLine("Aposentado");
+}
+else
+{
+    Console.WriteLine("Maior de Idade");
+}
---
+++ Aula_04_Exercicio_02/Program.cs
@@ -0,0 +1,55 @@
+﻿// Login simplificado
+
+string loginBD = "gabriel05";
+string passwordBD = "PWpadrao123";
+string nomeReduzidoCliente = "Gabriel Alexandre";
+double saldoContaCorrente = 1500.55d;
+const int LIMITE_TENTATIVAS = 3;
+
+Console.WriteLine("Olá! Seja-bem vindo ao banco Entra21. Insira o número da sua Conta e Senha entrar.");
+
+// Login Existe? + Até 3 tentativas incorretas
+string loginInput = Console.ReadLine();
+int tentativasLoginFalhado = 0;
+
+do
+{
+    Console.Write("Número da Conta: ");
+}
+while (loginInput != loginBD && tentativasLoginFalhado < LIMITE_TENTATIVAS);
+
+
+
+while (loginInput != loginBD && tentativasLoginFalhado < LIMITE_TENTATIVAS) ;
+{
+    Console.WriteLine("Conta inexistente! " + (LIMITE_TENTATIVAS - tentativasLoginFalhado) + " tentativa(s) restante(s).");
+    Console.Write("Número da Conta: ");
+    loginInput = Console.ReadLine();
+    if (tentativasLoginFalhado >= LIMITE_TENTATIVAS)
+    {
+        Console.WriteLine("Conta bloqueada por excesso de tentativas, contate o suporte.");
+        return;
+    }
+    tentativasLoginFalhado++;
+}
+
+// Senha Correta? + Até 3 tentativas incorretas
+Console.Write("Senha: ");
+string passwordInput = Console.ReadLine();
+int tentativasSenhaIncorreta = 0;
+
+while (passwordInput != passwordBD && tentativasSenhaIncorreta < LIMITE_TENTATIVAS)
+{
+    Console.WriteLine("Senha incorreta! " + (LIMITE_TENTATIVAS - tentativasSenhaIncorreta) + " tentativa(s) restante(s).");
+    Console.Write("Senha: ");
+    loginInput = Console.ReadLine();
+    if (tentativasSenhaIncorreta >= LIMITE_TENTATIVAS)
+    {
+        Console.WriteLine("Contate o suporte para fazer o reset da senha.");
+        return;
+    }
+    tentativasSenhaIncorreta++;
+}
+
+Console.WriteLine("Bem vindo, " + nomeReduzidoCliente + ".");
+Console.WriteLine("Saldo Conta Corrente: R$ " + saldoContaCorrente + ".");
