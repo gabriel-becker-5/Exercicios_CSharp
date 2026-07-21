0f2fbcdb9650803e84cc5567ffda133bd742d7f4
gabriel-becker-5 <gabriel.alexandre.becker@gmail.com>
2026-07-21 11:18:15 -0300

    exercicios-aula-7-mvc-finalizados

---
+++ Aula_MVC_04_Exercicios/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using Aula_MVC_04_Exercicios.Data;
+using Aula_MVC_04_Exercicios.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula_MVC_04_Exercicios.Controllers
+{
+    [Authorize]
+    [Route("admin")]
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminController(RoleManager<IdentityRole> rolemanager, 
+                               UserManager<ApplicationUser> usermanager)
+        {
+            _roleManager = rolemanager;
+            _userManager = usermanager;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult PainelAdmin()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost("atribuiruserrole")]
+        public async Task<IActionResult> AtribuirUserRole(AlterarRoleViewModel model)
+        {
+            ApplicationUser usuario = await _userManager.FindByEmailAsync(model.userEmail);
+            if (usuario == null) 
+            {
+                return View("PainelAdmin");
+            }
+            await _userManager.AddToRoleAsync(usuario, model.Role);
+            return View("PainelAdmin");
+        }
+    }
+}
---
+++ Aula_MVC_04_Exercicios/Models/AlterarRoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Aula_MVC_04_Exercicios.Models
+{
+    public class AlterarRoleViewModel
+    {
+        public string userEmail { get; set; }
+        public string Role {  get; set; }
+    }
+}
